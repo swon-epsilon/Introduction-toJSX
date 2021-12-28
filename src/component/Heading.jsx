2fa8ca8282a1c642737af04c42db6a1afd425d63
Sam Won <56573773+swon-epsilon@users.noreply.github.com>
2021-12-28 10:27:48 -0800

    Add js import

--- src/component/Heading.jsx
+++ src/component/Heading.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../public/styles.css";
 
 function Heading() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/App.jsx";
+import * as Calc from "./component/calculator.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+  <div>
+    <ul>
+      <li>{Calc.add(1, 2)}</li>
+    </ul>
+  </div>,
+  document.getElementById("root2")
+);
